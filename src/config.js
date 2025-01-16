026aff0015fcb99dd39c554e44bcfa8b5291f5f6
Patrick Niyogitare <54368821+thepatrickniyo@users.noreply.github.com>
2025-01-16 01:08:10 +0100

    ch(github-link): change redirect

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ module.exports = {
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/patrickniyogitare28',
+      url: 'https://github.com/thepatrickniyo',
     },
     {
       name: 'Twitter',
